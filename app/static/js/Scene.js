052906fa1fb4c6794a1bec66a6a3828fa04d2966
Matthew Chan <mchan00@stuy.edu>
2020-06-09 12:56:55 -0400

    Modularized js files

---
+++ app/static/js/Scene.js
@@ -0,0 +1,13 @@
+export default class Scene {
+    constructor() {
+        this.init();
+    }
+    init() {
+        return 0;
+    }
+    update() {
+        console.log("game loop is running fine");
+        return 0;
+    }
+    exit() {}
+}
