e851a2a11fa416a959e3e4a0a46487f4173b07b0
Matthew Chan <mchan00@stuy.edu>
2020-06-09 18:15:55 -0400

    Database work

--- app/utl/db.py
+++ app/utl/db.py
@@ -12,7 +12,7 @@ DEFAULT_HEIGHT = 15
 DEFAULT_WIDTH = 30
 
 #=============================================================================
-# WRAPPER FUNCTION(S)
+# WRAPPER FUNCTIONS
 
 # Connects to the database file
 # Passes the connected object to the wrapped function
@@ -26,8 +26,9 @@ def _connects(db_func):
             print(error)
             return False
     return establish_connection
-#=============================================================================
 
+#=============================================================================
+# DATABASE INTERACTION FUNCTIONS
 
 # Initialize the database with the necessary maps table
 @_connects
@@ -35,46 +36,72 @@ def init(db=None):
     # Create the maps table to store map data
     db.execute('''
                 CREATE TABLE IF NOT EXISTS maps(
-                    name TEXT UNIQUE PRIMARY KEY, 
+                    id INTEGER UNIQUE PRIMARY KEY,
                     height INTEGER,
                     width INTEGER,
-                    data INTEGER
+                    data TEXT
                );''')
     db.commit()
 
 
 # Retrieve the map data for a given map found by name
 @_connects
-def get_data(name, db=None):
+def db_get_map(id, db=None):
     results = db.execute('''
-                        SELECT data 
+                        SELECT * 
                         FROM maps 
-                        WHERE name=?;
+                        WHERE id=?;
                         ''', 
-                        (name,))
+                        (id,))
     return results.fetchone()
 
 
-# Retrieve a random map data
+# Retrieve a random map's data
 @_connects
-def get_rand_data(db=None):
+def db_get_rand_map(db=None):
     results = db.execute('''
-                        SELECT data 
+                        SELECT * 
                         FROM maps;
                         ''')
     results = [i for i in results]
     return random.choice(results)
 
 
+# Add a map to the database
 @_connects
-def add_map(name, height=DEFAULT_HEIGHT, width=DEFAULT_WIDTH, data, db=None):
+def db_add_map(id, height=DEFAULT_HEIGHT, width=DEFAULT_WIDTH, data, db=None):
     db.execute('''
                 INSERT INTO maps 
                 VALUES(?, ?, ?, ?);
                 ''',
-                (name, 
-                height, 
-                width, 
-                data
-                ))
+                (None, height, width, data))
     db.commit()
+
+#=============================================================================
+# DATA FORMATTING FUNCTIONS
+
+# Convert string map data into 2d array
+def data_convert(height, width, data):
+    map_array = list()
+    for i in range(height):
+        row = data[(i*width):(i*(width+1)+1)]
+        row = [int(i) for i in list(row)]
+
+        map_array.append(row)
+    return map_array
+
+
+# Return all formatted map information as dictionary
+def get_map(id=None):
+    if (id):
+        db_result = db_get_map(id)
+    else:
+        db_result = db_get_rand_map()
+    
+    map_dict = {
+        'name': 'level' + str(db_result[0]),
+        'height': db_result[1],
+        'width': db_result[2],
+        'data': data_convert(db_result[1], db_result[2], db_result[3])
+    }
+    return map_dict
