3424bc0edb23fed1897a6bf405b9bb1006f06673
Matthew Chan <mchan00@stuy.edu>
2020-06-10 00:04:14 -0400

    Map generation work

--- app/static/js/GameObjects/Tile.js
+++ app/static/js/GameObjects/Tile.js
@@ -1,19 +1,21 @@
-import Polygon from '../Physics/Polygon.js'
+import GameObject from '../GameObject.js';
+import Polygon from '../Physics/Polygon.js';
+
 export default class Tile extends GameObject {
-constructor(x, y, id) {
-    super();
-    this.x = x;
-    this.y = y;
-    this.tileID = id;
-    this.hitbox = new Polygon();
-    this.init();
-}
-init() {
-    return 0;
-}
-update() {
-    return 0;
-}
-render(ctx) {}
-exit() {}
+    constructor(x, y, id) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.tileID = id;
+        this.hitbox = new Polygon();
+        this.init();
+    }
+    init() {
+        return 0;
+    }
+    update() {
+        return 0;
+    }
+    render(ctx) {}
+    exit() {}
 }
