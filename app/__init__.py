63defdbf56429bec6d19f4afd3ae22322bbf0965
Matthew Chan <mchan00@stuy.edu>
2020-06-10 00:08:51 -0400

    Database get route added

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,6 @@ from utl import db
 
 
 app = Flask(__name__)
-
 # creates secret key for sessions
 app.secret_key = os.urandom(32)
 
@@ -20,6 +19,10 @@ app.secret_key = os.urandom(32)
 @app.route("/")
 def root():
     return render_template('game.html')
+
+@app.route("/map")
+def get_map():
+    return db.get_map()
 
 
 if __name__ == "__main__":
