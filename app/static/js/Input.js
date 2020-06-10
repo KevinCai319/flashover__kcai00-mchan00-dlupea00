f70d903a5cc647a09ea18e2f46073bc7b83fbfb9
DavidLupea <dlupea00@stuy.edu>
2020-06-10 12:45:34 -0400

    clicks

--- app/static/js/Input.js
+++ app/static/js/Input.js
@@ -2,17 +2,27 @@ export default class Scene {
     constructor(document) {
       this.canvas = document;
       addEvent(document, "keypress", keypress);
+      addEvent(document, "click", click);
     }
 
-    function keypress() {
+    keypress() {
       if (event.key == 'w') {
         console.log("w");
+        return 1;
       } else if (event.key == 'a') {
         console.log("a");
+        return 2;
       } else if (event.key == 's') {
         console.log("s");
+        return 3;
       } else if (event.key == 'd') {
         console.log("d");
+        return 4;
       }
+    }
+
+    click() {
+      console.log("click");
+      return 5;
     }
 }
