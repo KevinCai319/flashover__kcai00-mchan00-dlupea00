97adf79f813af879aa256505b0ab9a66a1506ffa
kevin16777216 <k1029384756c@gmail.com>
2020-06-10 23:10:05 -0400

    minor bugfixes

--- app/static/js/Scene.js
+++ app/static/js/Scene.js
@@ -1,45 +1,50 @@
-import GameObject from './GameObject.js'
+import GameObject from "./GameObject.js";
 export default class Scene {
-    objects = [new GameObject()];
-    //to be destroyed
-    tbd = [new GameObject()];
-    tbc = [new GameObject()];
-    constructor() {
-        this.objects = [];
-        this.tbd = [];
-        this.tbc = [];
-        this.init();
+  objects = [new GameObject()];
+  //to be destroyed
+  tbd = [new GameObject()];
+  tbc = [new GameObject()];
+  constructor() {
+    this.objects = [];
+    this.tbd = [];
+    this.tbc = [];
+    this.init();
+  }
+  init() {
+    return 0;
+  }
+  update(ctx) {
+    //remove old objects that were requested to be removed
+    while (this.tbd.length) {
+      this.objects.splice(this.tbd.pop(), 1);
     }
-    init() {
-        return 0;
+    //add new objects requested to be added
+    while (this.tbc.length) {
+      this.objects.push(this.tbc.pop());
     }
-    update(ctx) {
-        for(let i = this.objects.length; i > 0; i--){
-            let signal = this.objects[i].update();
-            if(signal){
-                this.handleSignal(signal);
-            }
-        }
-        while(tbd.length) {
-            this.objects.splice(tbd.pop(), 1);
-        }
-        while(tbc.length){
-            this.objects.push(tbc.pop());
-        }
-        for(let i = 0; i < this.objects.length; i++){
-            this.objects[i].render(ctx);
-        }
-        return 0;
+
+    //update objects
+    for (let i = this.objects.length - 1; i != -1; i--) {
+      let signal = this.objects[i].update();
+      if (signal) {
+        this.handleSignal(signal);
+      }
     }
-    push(obj){
-        this.tbc.push(obj);
-    }
-    destroy(idx){
-        this.tbd.push(idx);
-    }
-    log(msg){
-        console.log(msg);
-    }
-    handleSignal(){}
-    exit() {}
+    //render objects
+    this.objects.forEach((obj) => {
+      obj.render(ctx);
+    });
+    return 0;
+  }
+  push(obj) {
+    this.tbc.push(obj);
+  }
+  destroy(idx) {
+    this.tbd.push(idx);
+  }
+  log(msg) {
+    console.log(msg);
+  }
+  handleSignal() {}
+  exit() {}
 }
