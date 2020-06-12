fb0f1eb682768b0881d6b0e83a086359458acec2
DavidLupea <dlupea00@stuy.edu>
2020-06-12 17:32:42 -0400

    fpa fixes

--- app/static/js/Logic.js
+++ app/static/js/Logic.js
@@ -6,9 +6,16 @@ var ctx = canvas.getContext("2d");
 // Start things off
 var handler = new GameHandler(ctx);
 // var input = new Input(document)
+
+
 function main(time) {
     handler.update(ctx);
     // input.logKey()
-    requestAnimationFrame(main);
+    setTimeout(function() {
+      requestAnimationFrame(main);
+    }, 1000/35);
+    // This gets a relatively consistent frame rate of 20-30 with a few stutters above that
+    // Shooting still looks a little random but the framerate monitor shows really good consistency
 }
+
 requestAnimationFrame(main);
