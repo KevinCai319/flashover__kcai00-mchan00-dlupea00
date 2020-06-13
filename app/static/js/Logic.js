ab2ac2fc9204c1559b5a3744b18a52ed5e591f94
kevin16777216 <k1029384756c@gmail.com>
2020-06-12 23:14:16 -0400

    clean logic.js

--- app/static/js/Logic.js
+++ app/static/js/Logic.js
@@ -6,12 +6,9 @@ var handler = new GameHandler(ctx);
 
 function main(time) {
     handler.update(ctx);
-    // input.logKey()
     setTimeout(function() {
       requestAnimationFrame(main);
     }, 1000/60);
-    // This gets a relatively consistent frame rate of 20-30 with a few stutters above that
-    // Shooting still looks a little random but the framerate monitor shows really good consistency
 }
 
 requestAnimationFrame(main);
